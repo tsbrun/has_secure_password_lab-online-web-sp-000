d6d54205002a0923d750a6c1f9190dc8d01a765d
TSephanya Brun <tsbrun@icloud.com>
2020-12-28 13:26:28 -0500

    commit tree 3d1b3fa827c363b0d06b8d90186ad469e87463b6

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,16 @@
 class SessionsController < ApplicationController
+    def new
+        @user = User.new
+    end
+
     def create 
         @user = User.find_by(name: params[:user][:name])&.authenticate(params[:user][:password])
         if @user 
             session[:user_id] = @user.id
+            redirect_to user_path(@user)
         end
     end
 end
+
+# for some reason, instead of logging in, I keep creating new users
+# probably something wrong with the form
